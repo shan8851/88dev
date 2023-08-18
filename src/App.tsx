f45428fb34ca6ba2b2f49616824fd93e5fef2f85
shan5742 <asamshan456@gmail.com>
2023-08-18 18:39:23 +0100

    [dev] - dev

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      <h1>88 DEV</h1>
+    </>
+  )
+}
+
+export default App
