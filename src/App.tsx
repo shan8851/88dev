af2c8dad52ea508947589a45aa67e24603b1c26c
shan5742 <asamshan456@gmail.com>
2023-08-19 08:55:58 +0100

    [landing] - landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
   return (
-    <>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      <h1>88 DEV</h1>
-    </>
+      <div className="container">
+        <h3 className="title">EightyEight</h3>
+        <h2 className="sub">DEV LTD</h2>
+      </div>
   )
 }
 
